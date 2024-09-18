959b4ee640ca496e8771c27b959e3aa075f3220e
Saad Does Code <saad.does.code@gmail.com>
2024-09-18 23:13:18 +0500

    Made Super User and Registered Models

--- Django/Django Part 1/project_1/first_app/admin.py
+++ Django/Django Part 1/project_1/first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from first_app.models import Topic, AccessDate, WebPage
 
 # Register your models here.
+admin.site.register(Topic)
+admin.site.register(AccessDate)
+admin.site.register(WebPage)
